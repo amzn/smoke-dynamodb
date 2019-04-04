52968cd609d6603f1465a26cae00f7c480685d2d
Simon Pilkington <simonpi@amazon.com>
2019-04-04 09:58:07 -0700

    Cleanup Package.swift.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,6 @@ import PackageDescription
 let package = Package(
     name: "SmokeDynamoDB",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SmokeDynamoDB",
             targets: ["SmokeDynamoDB"]),
@@ -29,8 +28,6 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SmokeDynamoDB",
             dependencies: ["LoggerAPI", "DynamoDBClient", "SmokeHTTPClient"]),
