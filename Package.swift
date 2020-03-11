0e4beb0cc1dccbbc3f7f127df7fb3e819c370638
Simon Pilkington <simonmpilkington@icloud.com>
2020-03-11 14:46:12 -0700

    Relax version requirements for SwiftMetrics.

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .package(url: "https://github.com/amzn/smoke-aws.git", from: "2.0.0-alpha.4"),
         .package(url: "https://github.com/amzn/smoke-http.git", from: "2.0.0-alpha"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-metrics.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0"..<"3.0.0"),
     ],
     targets: [
         .target(
