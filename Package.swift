f9fc9663d1e91824088fd98921bc72f8794dd0d0
Simon Pilkington <simonmpilkington@icloud.com>
2020-04-12 12:01:38 -0700

    Update to beta dependency requirements.

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
             targets: ["SmokeDynamoDB"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/amzn/smoke-aws.git", from: "2.0.0-alpha.6"),
-        .package(url: "https://github.com/amzn/smoke-http.git", from: "2.0.0-alpha.9"),
+        .package(url: "https://github.com/amzn/smoke-aws.git", from: "2.0.0-beta.1"),
+        .package(url: "https://github.com/amzn/smoke-http.git", from: "2.0.0-beta.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0"..<"3.0.0"),
     ],
